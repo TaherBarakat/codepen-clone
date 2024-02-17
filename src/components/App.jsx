51f6f31bc136d7a8bbdbbeb1f784b24ec81cbaef
Taher Barakat <taher.barakat12223@gmail.com>
2024-02-17 18:40:43 +0300

    apply functionality to the open/close button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -45,7 +45,7 @@ function App() {
                     <iframe
                          srcDoc={srcDoc}
                          title="output"
-                         sandbox="allow-script"
+                         sandbox="allow-scripts"
                          frameBorder="0"
                          width="100%"
                          height="100%"
--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -4,16 +4,20 @@ import "codemirror/mode/xml/xml";
 import "codemirror/mode/javascript/javascript";
 import "codemirror/mode/css/css";
 import { Controlled as ControlledEditor } from "react-codemirror2";
+import { useState } from "react";
 
 export default function Editor({ displayName, language, value, onChange }) {
+     const [open, setOpen] = useState(true);
      function handleChange(editor, data, value) {
           onChange(value);
      }
      return (
-          <div className="editor-container">
+          <div className={`editor-container ${open ? "" : "collapsed"} `}>
                <div className="editor-title">
                     {displayName}
-                    <button>O/C</button>
+                    <button onClick={() => setOpen((prev) => !prev)}>
+                         O/C
+                    </button>
                </div>
                <ControlledEditor
                     onBeforeChange={handleChange}
